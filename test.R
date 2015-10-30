0e44a93344ee71b2a0312a38826a0ca7b459af4d
miles0302 <weilin0929@gmail.com>
2015-10-29 21:55:16 -0700

    Calculating Information Gain

--- test.R
+++ test.R
@@ -14,8 +14,6 @@ cvData <- rawData[-splitData,]
 
 # Label
 y <- trainingData[,'Survived']
-# Choose 'Sex' as one Feature
-X <- trainingData[,'Sex']
 
 # test using the cvData
 cvLabel <- rep(0,nrow(cvData))
@@ -31,3 +29,30 @@ testLabel[testData[,'Sex'] == 'female','Survived'] <- 1
 
 # Output to csv file
 write.csv(testLabel,file = 'submission.csv',row.names = FALSE)
+
+# Calculate Information Gain using 'Sex'
+# Entropy for parent
+p1 = sum(y)/length(y)
+p0 = 1 - p1
+entropyParent = -p1*log2(p1) - p0*log2(p0)
+
+# Choose 'Sex' as a Feature
+Feature <- 'Sex'
+# Choose 'Pclass'as a Feature
+Feature <- 'Pclass'
+# Choose 'Embarked'
+# Feature <- 'Embarked'
+# trainingData[trainingData[,Feature]=="",Feature] = "S"
+
+X <- as.factor(trainingData[,Feature])
+
+# Entropy for each class in X
+trainingTable  <- prop.table(table(X,y),1)
+classEntropy = rowSums(-trainingTable*log2(trainingTable))
+# Children Frequency
+classFreq <- prop.table(table(X))
+# Entropy for children
+entropyChildren = sum(classEntropy*classFreq)
+# Information Gain using X
+IG = entropyParent - entropyChildren
+print(IG)
